1c9da5cfbd0990f04e6858a286f4c37194cc8754
Emry Brisky <eabrisky@gmail.com>
2021-04-18 17:09:19 -0500

    initial commit

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,42 +6,89 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i=0; i<4; i++) {
   console.log(i);
 }
 
-// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
+// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+
+for (let i=0; i<6; i++) {
+  console.log(i);
+}
+
+// you need to change i<4 to i<6 so it allows the loop to continue incrementing the value of i past 3 and up to 5, stopping at 6.
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 
+for (let i=3; i<6; i++) {
+  console.log(i);
+}
+
+// you need to change the initial value of i to 3 so it starts the loop there.
+
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+for(let i=2; i<12; i+=2) {
+  console.log(i);
+}
+
+// you need to change your i++ to an i+=2 in order to add 2 to its value with each loop instead of adding only 1,
+// change your beginning i value to 2, and have the array end at 12
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
+for (let i=5; i>-1; i--) {
+  console.log(i);
+}
 
-// 2. For of loops  
+// 2. For of loops
 console.log('---- 2. For Of loop ----');
-let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
+let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'];
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
-
-// 3. While loops  
+for(let name of stars){
+  console.log(name);
+}
+// 3. While loops
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+let habitablePlanets=1;
+let terraformKits=2;
+
+while(habitablePlanets<terraformKits){
+  console.log(stars);
+  habitablePlanets++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+let index=0;
+let max=6;
+
+while(index<max){
+  console.log(index);
+  index++;
+}
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+let indus=10;
+let mouse=4;
+
+while(indus>mouse){
+  console.log(indus);
+  indus--;
+}
